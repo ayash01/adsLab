e3829f041da7516af33243cea099a14a15d83cdf
ayash01 <2231@tkmce.ac.in>
2022-11-29 15:13:27 +0530

    new commit

--- circular_linked_list.c
+++ circular_linked_list.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void display(), insertBeg(), insertEnd(), insertBet(), deleteBeg(), deleteEnd(), insertBet(), deleteBet();
-
 struct node
 {
     int data;
@@ -10,47 +8,6 @@ struct node
 };
 
 struct node *head = NULL;
-
-void main()
-{
-    // choice
-    int ch;
-    do
-    {
-        printf("\nEnter choice:\n1. Insert node at the beginning\n2. Insert node at the end\n3. Insert node in between\n4. Display List\n5. Delete node from the beginning\n6. Delete node from the end\n7. Delete specified node\n\n0: Exit\n");
-        printf("\n-> ");
-        scanf("%d", &ch);
-        printf("\n");
-        switch (ch)
-        {
-        case 1:
-            insertBeg();
-            break;
-        case 2:
-            insertEnd();
-            break;
-        case 3:
-            insertBet();
-            break;
-        case 4:
-            printf("Current List:\n");
-            display();
-            printf("\n");
-            break;
-        case 5:
-            deleteBeg();
-            break;
-        case 6:
-            deleteEnd();
-            break;
-        case 7:
-            deleteBet();
-            break;
-        default:
-            printf("\n");
-        }
-    } while (ch != 0);
-}
 
 void insertBeg() {
 
@@ -130,4 +87,45 @@ void display()
         printf("%d  ", ptr->data);
         ptr = ptr->next;
     }
+}
+
+void main()
+{
+    // choice
+    int ch;
+    do
+    {
+        printf("\nEnter choice:\n1. Insert node at the beginning\n2. Insert node at the end\n3. Insert node in between\n4. Display List\n5. Delete node from the beginning\n6. Delete node from the end\n7. Delete specified node\n\n0: Exit\n");
+        printf("\n-> ");
+        scanf("%d", &ch);
+        printf("\n");
+        switch (ch)
+        {
+        case 1:
+            insertBeg();
+            break;
+        case 2:
+            insertEnd();
+            break;
+        case 3:
+            insertBet();
+            break;
+        case 4:
+            printf("Current List:\n");
+            display();
+            printf("\n");
+            break;
+        case 5:
+            deleteBeg();
+            break;
+        case 6:
+            deleteEnd();
+            break;
+        case 7:
+            deleteBet();
+            break;
+        default:
+            printf("\n");
+        }
+    } while (ch != 0);
 }
