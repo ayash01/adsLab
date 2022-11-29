dae1cd31ae3b455f1375f6b9fbcb41fb3efa3680
unknown <2231@tkmce.ac.in>
2022-11-29 13:50:04 +0530

    add new

---
+++ circular_linked_list.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void display(), insertBeg(), insertEnd(), insertBet(), deleteBeg(), deleteEnd(), insertBet(), deleteBet();
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *head = NULL;
+
+void main()
+{
+    // choice
+    int ch;
+    do
+    {
+        printf("\nEnter choice:\n1. Insert node at the beginning\n2. Insert node at the end\n3. Insert node in between\n4. Display List\n5. Delete node from the beginning\n6. Delete node from the end\n7. Delete specified node\n\n0: Exit\n");
+        printf("\n-> ");
+        scanf("%d", &ch);
+        printf("\n");
+        switch (ch)
+        {
+        case 1:
+            insertBeg();
+            break;
+        case 2:
+            insertEnd();
+            break;
+        case 3:
+            insertBet();
+            break;
+        case 4:
+            printf("Current List:\n");
+            display();
+            printf("\n");
+            break;
+        case 5:
+            deleteBeg();
+            break;
+        case 6:
+            deleteEnd();
+            break;
+        case 7:
+            deleteBet();
+            break;
+        default:
+            printf("\n");
+        }
+    } while (ch != 0);
+}
+
+void insertBeg() {
+
+}
+
+void insertEnd() {
+
+}
+
+void insertBet()
+{
+    int data, pos;
+    printf("Enter data to insert: ");
+    scanf("%d", &data);
+
+    printf("\nEnter element to insert after: ");
+    scanf("%d", &pos);
+
+    struct node *temp = (struct node *)malloc(sizeof(struct node));
+    temp->data = data;
+    temp->next = NULL;
+    struct node *ptr = head;
+    if (ptr == NULL)
+    {
+        temp->next = head;
+        head = temp;
+    }
+    else
+    {
+        while (ptr->data != pos)
+        {
+            ptr = ptr->next;
+        }
+        temp->next = ptr->next;
+        ptr->next = temp;
+    }
+}
+
+void deleteBeg() {
+
+}
+
+void deleteEnd() {
+
+}
+
+void DeleteBet()
+{
+    int pos;
+    printf("Which element to delete?: ");
+    scanf("%d", &pos);
+
+    struct node *ptr = head, *temp;
+
+    if (ptr == NULL)
+    {
+        printf("Underflow!");
+    }
+    else
+    {
+        while (ptr->data != pos)
+        {
+            temp = ptr;
+            ptr = ptr->next;
+        }
+        temp->next = ptr->next;
+        free(ptr);
+    }
+}
+
+void display()
+{
+    struct node *ptr = head;
+
+    while (ptr != NULL)
+    {
+        printf("%d  ", ptr->data);
+        ptr = ptr->next;
+    }
+}
