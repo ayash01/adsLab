e6d12d92f9653ab8d634cbbe702a2c47035b0be3
ayash01 <2231@tkmce.ac.in>
2023-01-20 00:15:33 +0530

    rewrite circular queue

--- Circular Queue/circular-q.c
+++ Circular Queue/circular-q.c
@@ -1,89 +1,99 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 10
+#define N 5
 
-int cq[N], front = -1, rear = -1;
+int queue[N], front = -1, rear = -1;
 
-void enq()
-{
-	int data;
-	printf("Enter element to insert: ");
-	scanf("%d", &data);
+void enqueue() {
+    int data;
+    printf("Enter data to enqueue: ");
+    scanf("%d", &data);
 
-	if ((rear + 1) % N == front)
-		printf("Overflow\n");
-	else if (front == -1 && rear == -1)
-	{
-		front = 0;
-		rear = 0;
-		cq[rear] = data;
-	}
-	else
-	{
-		rear = (rear + 1) % N;
-		cq[rear] = data;
-	}
+    if ((rear+1) % N == front) {
+        printf("Queue Full");
+    }
+
+    else {
+        if (front == -1 && rear == -1) {
+            front = 0;
+            rear = 0;
+        }
+
+        else {
+            rear = (rear+1) % N;
+        }
+
+        queue[rear] = data;
+    }
 }
 
-void deq()
-{
-	int data;
-	if (front == -1 && rear == -1)
-		printf("Queue Empty\n");
-	else if (front == rear)
-	{
+void dequeue() {
+    int data = queue[front];
 
-		data = cq[front];
-		front = -1;
-		rear = -1;
-		printf("Element %d dequeued\n", data);
-	}
-	else
-	{
-		data = cq[front];
-		front = (front + 1) % N;
-		printf("ELement %d dequeued\n", data);
-	}
+    if (front == -1 && rear == -1) {
+        printf("Queue Empty");
+    }
+
+    else { 
+        if (front == rear) {
+            front = -1;
+            rear = -1;  
+        }
+        else {
+            front = (front+1) % N;
+        }
+        printf("Element %d dequeued", data);
+    }
 }
 
-void display(void){
+void display()
+{
+    // case if queue empty
+    if (front == -1 && rear == -1) {
+        printf("Queue is empty");
+    }
 
-	if(front == -1 && rear == -1)
-		printf("Queue Empty\n");
-	else{
-		for(int i = front; i <= rear; i++)
-			printf("%d  ",cq[i]);
-		printf("\n");
-	}
+    // default case
+    else {
+        printf("Current Queue:\n");
+        for (int i = front; i <= rear; i++) {
+            printf("%d  ", queue[i]);
+        }
+        printf("\nFront: %d\nRear: %d", queue[front], queue[rear]);
+    }
 }
 
 void main()
 {
-	// choice
-	int ch;
-	do
-	{
-		printf("Enter choice:\n1. Enqueue\n2. Dequeue\n3. Display\n0: Exit\n");
-		printf("\n-> ");
-		scanf("%d", &ch);
-		printf("\n");
-		switch (ch)
-		{
-		case 1:
-			enq();
-			printf("\n");
-			break;
-		case 2:
-			deq();
-			printf("\n");
-			break;
-		case 3:
-			display();
-			printf("\n");
-			break;
-		default:
-			printf("\n");
-		}
-	} while (ch != 0);
+    // choice
+    int ch;
+    do
+    {
+        printf("\n");
+        printf("Enter choice:\n1. Enqueue\n2. Dequeue\n\n3. Display\n\n0: Exit\n");
+        printf("\n-> ");
+        scanf("%d", &ch);
+        printf("\n");
+        switch (ch)
+        {
+        case 0:
+            printf("Exiting program...");
+            exit(0);
+        case 1:
+            enqueue();
+            printf("\n");
+            break;
+        case 2:
+            dequeue();
+            printf("\n");
+            break;
+        case 3:
+            display();
+            printf("\n");
+            break;
+        default:
+            printf("\n");
+        }
+    } while (ch != 0);
 }
